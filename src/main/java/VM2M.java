0fd25f79a28805bf5d81f507158da73064e9885a
Ari Sweedler <ari@sweedler.com>
2018-12-08 13:06:09 -0800

    Added visitors into VM2M

--- src/main/java/VM2M.java
+++ src/main/java/VM2M.java
@@ -1,33 +1,108 @@
 import cs132.util.ProblemException;
+import cs132.vapor.ast.*;
 import cs132.vapor.parser.VaporParser;
-import cs132.vapor.ast.VaporProgram;
 import cs132.vapor.ast.VBuiltIn.Op;
 import java.io.*;
 
-public class VM2M {
+public class VM2M extends VInstr.Visitor<RuntimeException> {
     
     public static void main (String [] args) {
         try {
             FileInputStream fs = new FileInputStream(
                   "/Users/ari/Desktop/cs132/testcases/hw5/BinaryTree.vaporm"
             );
-        } catch (Exception e) {
-            System.out.println(e);
-            System.out.println("Error");
-        }
+            VaporProgram programAST = parseVapor(fs, System.out);
 
-        VaporProgram programAST;
-        try {
-            programAST = parseVapor(System.in, System.out);
+            compile_programStart(programAST);
+            for (VFunction func: programAST.functions) {
+                visit(func);
+            }
+            compile_programEnd();
+
         } catch (IOException e) {
             System.out.println("AST parse error:");
             System.out.println(e);
         }
 
         /* use the AST to translate the program */
+        /* visit the AST */
+    }
 
+    private static void visit(VFunction func) {
+        int itemsOnStack = (func.stack.local + func.stack.out);
+        itemsOnStack += 2; /* returns addr & old value of $fp */
+        int stackSize = itemsOnStack * 4;
 
+        compile_functionStart(stackSize);
+        for (VInstr instr: func.body) {
+            //TODO visit each instr
+            /* check ALL */
+        }
+        compile_functionEnd(stackSize);
+    }
 
+    private static void compile_functionStart(int stackSize) {
+        String boilerplate = "  sw $fp -8($sp)\n" +
+              "  move $fp $sp\n" +
+              "  subu $sp $sp %d\n" +
+              "  sw $ra -4($fp)";
+        System.out.printf(boilerplate, stackSize);
+    }
+
+    private static void compile_functionEnd(int stackSize) {
+        String boilerplate = "  lw $ra -4($fp)\n" +
+              "  lw $fp -8($fp)\n" +
+              "  addu $sp $sp %d\n" +
+              "  jr $ra";
+        System.out.printf(boilerplate, stackSize);
+    }
+
+    private static void compile_programStart(VaporProgram programAST)
+    {
+        /* print data segment */
+        System.out.println(".data");
+        System.out.println();
+        for (VDataSegment v: programAST.dataSegments) {
+            System.out.println(v); //TODO stuff
+        }
+
+        /* start the Main function */
+        System.out.println(".text");
+        System.out.println();
+        String s = "\n" +
+              "  jal Main\n" +
+              "  li $v0 10\n" +
+              "  syscall\n";
+        System.out.println(s);
+    }
+
+    private static void compile_programEnd()
+    {
+        String boilerplate = "_print:\n" +
+              "  li $v0 1   # syscall: print integer\n" +
+              "  syscall\n" +
+              "  la $a0 _newline\n" +
+              "  li $v0 4   # syscall: print string\n" +
+              "  syscall\n" +
+              "  jr $ra\n" +
+              "\n" +
+              "_error:\n" +
+              "  li $v0 4   # syscall: print string\n" +
+              "  syscall\n" +
+              "  li $v0 10  # syscall: exit\n" +
+              "  syscall\n" +
+              "\n" +
+              "_heapAlloc:\n" +
+              "  li $v0 9   # syscall: sbrk\n" +
+              "  syscall\n" +
+              "  jr $ra\n" +
+              "\n" +
+              ".data\n" +
+              ".align 0\n" +
+              "_newline: .asciiz \"\\n\"\n" +
+              "_str0: .asciiz \"null pointer\\n\"\n" +
+              "_str1: .asciiz \"array index out of bounds\\n\"";
+        System.out.println(boilerplate);
     }
 
     public static VaporProgram parseVapor(InputStream in, PrintStream err)
@@ -59,6 +134,46 @@ public class VM2M {
         }
 
         return program;
+    }
+
+    @Override
+    public void visit(VAssign a) {
+
+    }
+
+    @Override
+    public void visit(VCall vCall) throws RuntimeException {
+
+    }
+
+    @Override
+    public void visit(VBuiltIn vBuiltIn) throws RuntimeException {
+
+    }
+
+    @Override
+    public void visit(VMemWrite vMemWrite) throws RuntimeException {
+
+    }
+
+    @Override
+    public void visit(VMemRead vMemRead) throws RuntimeException {
+
+    }
+
+    @Override
+    public void visit(VBranch vBranch) throws RuntimeException {
+
+    }
+
+    @Override
+    public void visit(VGoto vGoto) throws RuntimeException {
+
+    }
+
+    @Override
+    public void visit(VReturn vReturn) throws RuntimeException {
+
     }
 
 }
